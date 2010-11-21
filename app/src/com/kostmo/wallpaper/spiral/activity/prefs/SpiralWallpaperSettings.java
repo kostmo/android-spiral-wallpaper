743b824e59d6ba472ea2935a876409174c75ac72
kostmo <kostmo@1bc94884-069a-9e9f-b792-bdb880735274>
2010-11-21 01:03:07 +0000

    Added hires promo image

--- app/src/com/kostmo/wallpaper/spiral/activity/prefs/SpiralWallpaperSettings.java
+++ app/src/com/kostmo/wallpaper/spiral/activity/prefs/SpiralWallpaperSettings.java
@@ -237,7 +237,7 @@ public class SpiralWallpaperSettings extends PreferenceActivity implements Color
 			})
 			.setNeutralButton(R.string.download_color_picker_web, new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int whichButton) {
-					startActivity(new Intent(Intent.ACTION_VIEW, Market.APK_DOWNLOAD_URI_COLOR_PICKER));
+					startActivity(new Intent(Intent.ACTION_VIEW, Market.APK_DOWNLOAD_URI));
 				}
 			})
 			.create();
